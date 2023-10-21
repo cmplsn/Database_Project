bca5425e0e7d3cfba8dca3c7660937a87ba392ba
alessandrocampagnaro <cmplsn97@gmail.com>
2023-10-21 17:38:35 +0200

    aggiornato schema. aggiunto 'status' a PROJECT

--- db_Schema/014-PROJECT.sql
+++ db_Schema/014-PROJECT.sql
@@ -3,7 +3,8 @@ create table "PROJECT"
     uuid        uuid default project_schema.uuid_generate_v4() not null
         primary key,
     title       varchar                                        not null,
-    description text
+    description text,
+    status      project_schema.evaluations_enum                not null
 );
 
 alter table "PROJECT"
@@ -12,4 +13,3 @@ alter table "PROJECT"
 grant insert, select, update on "PROJECT" to evaluator;
 
 grant insert, select, update on "PROJECT" to researcher;
-
