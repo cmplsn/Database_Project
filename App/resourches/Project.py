7938bd10652a048ed8895790c2e25de624380f3e
Elia Corò <43368654+EliaCoro@users.noreply.github.com>
2023-12-06 15:51:43 +0100

    inizio pagina file_page

--- App/resourches/Project.py
+++ App/resourches/Project.py
@@ -4,8 +4,8 @@ import requests
 from flask import *
 from flask_login import *
 from sqlalchemy import *
-from models import *
-from db import resSess, adminSess
+from App.models import *
+from App.db import resSess, adminSess
 
 prj_route = Blueprint('prj_route', __name__)
 
@@ -35,3 +35,22 @@ def prj_private(prj_id):
         print(e)
         resSess.rollback()
     return redirect(url_for('res_route.res_private'))
+
+
+@prj_route.route('/file_page/<uuid_file>', methods=['GET', 'POST'])
+def file_page(uuid_file):
+    #try:
+        if request.method == 'GET':
+            # TODO: Aggiungere controllo se l'utente ha accesso a quel file
+            file = resSess.execute(select(File).where(File.uuid == uuid_file)).fetchone().File
+            print("AAAAA")
+            print(file.versions[0].reports)
+            return render_template('File.html', file=file)
+        elif request.method == 'POST':
+            new_prj = Project(title=request.form['title'], description=request.form['description'])
+            resSess.add(new_prj)
+            resSess.commit()
+    #except Exception as e:
+        #print(e)
+        #resSess.rollback()
+    #return redirect(url_for('res_route.res_private'))
